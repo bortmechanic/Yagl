e6585f61cd3b8ddb6b87e7e83962924da6e96b37
bortmechanic <pavel.v.melnikov@gmail.com>
2020-09-12 20:07:21 +0300

    Sound Format Registry.

---
+++ Audio.Sound/FormatRegistry.cs
@@ -0,0 +1,32 @@
+/*
+ This source file is a part of the project YAGL.
+ Copyright (c) 2020 Pavel Melnikov.
+ Distributed under the MIT License (http://opensource.org/licenses/MIT).
+ See LICENSE.txt for the full license text.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yagl.Audio
+{
+    public static class FormatRegistry
+    {
+        private static readonly List<IFormat> Formats = new List<IFormat>();
+        
+        public static void Register(IFormat format)
+        {
+            Formats.Add(format);
+        }
+
+        public static void Clear()
+        {
+            Formats.Clear();
+        }
+        
+        internal static IFormat FindFormat(string filename)
+        {
+            return Formats.FirstOrDefault(f => f.CanProcess(filename));
+        }
+    }
+}
--- Audio.Sound/Sound.cs
+++ Audio.Sound/Sound.cs
@@ -1,3 +1,13 @@
+/*
+ This source file is a part of the project YAGL.
+ Copyright (c) 2020 Pavel Melnikov.
+ Distributed under the MIT License (http://opensource.org/licenses/MIT).
+ See LICENSE.txt for the full license text.
+*/
+
+using System;
+using System.IO;
+
 namespace Yagl.Audio
 {
     public class Sound
@@ -6,5 +16,23 @@ namespace Yagl.Audio
         public int SampleRate { get; set; }
         public int BitsPerSample { get; set; }
         public byte[] Data { get; set; }
+        
+        public static Sound Load(string filename)
+        {
+            var format = FormatRegistry.FindFormat(filename);
+            if (format == null)
+                throw new NotSupportedException($"Sound format of the file '{filename}' is not supported.");
+            using var stream = File.OpenRead(filename);
+            return format.Load(stream);
+        }
+
+        public void Save(string filename)
+        {
+            var format = FormatRegistry.FindFormat(filename);
+            if (format == null)
+                throw new NotSupportedException($"No suitable encoder found to save the sound '{filename}'.");
+            using var stream = File.OpenWrite(filename);
+            format.Save(this, stream);
+        }
     }
 }
